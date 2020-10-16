713cc8311cd7e67d7d5005c169cf9917a81f81a7
r-vu <46875289+r-vu@users.noreply.github.com>
2020-10-15 18:01:02 -0700

    Create basic item unit tests

---
+++ tests/Berg.Tests/ItemTest.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+
+namespace Berg.Tests {
+    public class ItemTest {
+
+        [Fact]
+        public void ItemCreateTest() {
+
+        }
+
+        [Fact]
+        public void ItemEditTest() {
+
+        }
+
+        [Fact]
+        public void ItemDeleteTest() {
+
+        }
+    }
+}

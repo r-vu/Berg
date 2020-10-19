1a0fd011c6fe15ba05de5853ed6a4bba2e754291
r-vu <46875289+r-vu@users.noreply.github.com>
2020-10-18 17:04:21 -0700

    Create TestUtilities

---
+++ tests/Berg.Tests/TestUtilities.cs
@@ -0,0 +1,35 @@
+﻿using Berg.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.IO;
+using System.Text;
+
+namespace Berg.Tests {
+    public static class TestUtilities {
+
+        public static Random RNG = new Random(123456789);
+
+        public static DbContextOptions<BergContext> TestDbContextOptions() {
+            ServiceProvider serviceProvider = new ServiceCollection()
+                .AddEntityFrameworkInMemoryDatabase()
+                .BuildServiceProvider();
+
+            DbContextOptionsBuilder<BergContext> builder = new DbContextOptionsBuilder<BergContext>()
+                .UseInMemoryDatabase("BergTestDb")
+                .UseInternalServiceProvider(serviceProvider);
+
+            return builder.Options;
+        }
+
+        public static string RandomString() {
+            int length = RNG.Next(3, 12);
+            return Path.GetRandomFileName().Replace(".", "").Substring(0, length);
+        }
+
+        public static int RandomInteger(int lower, int upper) {
+            return RNG.Next(lower, upper);
+        }
+
+    }
+}

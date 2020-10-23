7fe581352e74a510c55ab3498f5aa719197cb3e2
r-vu <46875289+r-vu@users.noreply.github.com>
2020-10-23 07:18:33 -0700

    Create preview of items on homepage

--- src/Berg/Pages/Index.cshtml.cs
+++ src/Berg/Pages/Index.cshtml.cs
@@ -1,16 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Berg.Data;
+using Berg.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Berg.Pages {
     public class IndexModel : PageModel {
         private readonly ILogger<IndexModel> _logger;
+        private readonly BergContext _context;
 
-        public IndexModel(ILogger<IndexModel> logger) {
+        public IList<Item> ItemList { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger, BergContext context) {
             _logger = logger;
+            _context = context;
         }
 
-        public void OnGet() {
-
+        public async Task OnGetAsync() {
+            IQueryable<Item> items = (from item in _context.Item select item);
+            int limit = await items.CountAsync();
+            limit = limit > 6 ? 6 : limit;
+            ItemList = await items.Skip(0).Take(limit).ToListAsync();
         }
     }
 }

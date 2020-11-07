328b5a4ffebfdc3673c359142605768b80a50487
r-vu <46875289+r-vu@users.noreply.github.com>
2020-11-06 17:11:13 -0800

    Modify layout of Search page
    Starting to implement 3x3 grid of search results. Navigation links for
    next and previous pages will be served using a method within the model
    for logic. Display logic will likely be in the CSHTML file.

--- src/Berg/Pages/Search.cshtml.cs
+++ src/Berg/Pages/Search.cshtml.cs
@@ -1,29 +1,33 @@
-using System;
+using Berg.Data;
+using Berg.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Berg.Data;
-using Berg.Models;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
 
-namespace Berg.Pages
-{
-    public class SearchModel : PageModel
-    {
+namespace Berg.Pages {
+    public class SearchModel : PageModel {
         private readonly BergContext Context;
 
         public IList<Item> ItemList { get; set; }
 
-        [FromQuery(Name = "keywords")]
+        [FromQuery(Name = "query")]
         public string SearchString { get; set; }
 
         [FromQuery(Name = "size")]
-        public int PageSize { get; set; } = 10;
+        public int PageSize { get; set; } = 9;
 
-        [FromQuery(Name = "page")]
-        public int PageNumber { get; set; } = 1;
+        [FromQuery(Name = "pg")]
+        public int CurrentPage { get; set; } = 1;
+        public int MaxPage { get; private set; }
+
+        private static readonly string QUERY_SEARCHSTRING = "query";
+        private static readonly string QUERY_PAGESIZE = "size";
+        private static readonly string QUERY_CURRENTPAGE = "pg";
 
         public SearchModel(BergContext context) {
             Context = context;
@@ -31,8 +35,26 @@ namespace Berg.Pages
 
         public async Task OnGetAsync() {
             IQueryable<Item> query = from item in Context.Item select item;
-            ItemList = await query.Where(item => item.Name.Contains(SearchString))
-                .Skip((PageNumber - 1) * PageSize).Take(PageSize).ToListAsync();
+            query = query.Where(item => item.Name.Contains(SearchString));
+            MaxPage = ((query.Count() - 1) / PageSize) + 1;
+            ItemList = await query.Skip((CurrentPage - 1) * PageSize).Take(PageSize).ToListAsync();
+        }
+
+        public string PageNavigationLink(bool next) {
+            RouteValueDictionary routeValueDictionary = Request.RouteValues;
+            IQueryCollection currentQuery = HttpContext.Request.Query;
+
+            foreach (string key in currentQuery.Keys) {
+                routeValueDictionary.Add(key, currentQuery[key]);
+            }
+
+            int pageNum = next ? CurrentPage + 1 : CurrentPage - 1;
+
+            if (!routeValueDictionary.TryAdd(QUERY_CURRENTPAGE, pageNum)) {
+                routeValueDictionary[QUERY_CURRENTPAGE] = pageNum;
+            }
+
+            return Url.RouteUrl(routeValueDictionary);
         }
     }
 }

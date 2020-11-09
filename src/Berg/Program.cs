ff2941908f44900cd5f0bf3895aa8653c5d03817
r-vu <46875289+r-vu@users.noreply.github.com>
2020-11-09 11:15:36 -0800

    Add images to default items

--- src/Berg/Program.cs
+++ src/Berg/Program.cs
@@ -25,16 +25,36 @@ namespace Berg {
         public static void Main(string[] args) {
 
             ITEM_LIST = new List<Item> {
-                new Item("Shrimp", 5.00M),
-                new Item("Canned Tuna", 2.00M),
-                new Item("Salmon", 10.00M),
-                new Item("Tilapia", 2.50M),
-                new Item("Alaska Pollock", 7.00M),
-                new Item("Catfish", 3.00M),
-                new Item("Crab", 12.00M),
-                new Item("Lobster", 20.00M),
-                new Item("Clams", 4.50M),
-                new Item("Scallops", 13.00M)
+                new Item("Shrimp", 5.00M) {
+                    Image = new Uri("https://images.unsplash.com/photo-1578069744397-2f3942a02a7b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80")
+                },
+                new Item("Canned Tuna", 2.00M) {
+                    Image = new Uri("https://images.freeimages.com/images/large-previews/9eb/tuna-can-1524738.jpg")
+                },
+                new Item("Salmon", 10.00M) {
+                    Image = new Uri("https://images.unsplash.com/photo-1599084993091-1cb5c0721cc6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80")
+                },
+                new Item("Tilapia", 2.50M) {
+                    Image = new Uri("https://images.freeimages.com/images/large-previews/181/tilapia-fish-1398846.jpg")
+                },
+                new Item("Alaska Pollock", 7.00M) {
+                    Image = new Uri("https://upload.wikimedia.org/wikipedia/commons/c/c4/Theragra_chalcogramma.png")
+                },
+                new Item("Catfish", 3.00M) {
+
+                },
+                new Item("Crab", 12.00M) {
+                    Image = new Uri("https://images.unsplash.com/photo-1553659971-f01207815844?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80")
+                },
+                new Item("Lobster", 20.00M) {
+                    Image = new Uri("https://images.unsplash.com/photo-1551463682-189bf47449d0?ixlib=rb-1.2.1&auto=format&fit=crop&w=1286&q=80")
+                },
+                new Item("Clams", 4.50M) {
+                    Image = new Uri("https://images.unsplash.com/photo-1599255460159-34c50585e1cb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80")
+                },
+                new Item("Scallops", 13.00M) {
+                    Image = new Uri("https://upload.wikimedia.org/wikipedia/commons/8/8b/Scallops_with_wine_sauce.jpg")
+                }
             };
 
             IHost host = CreateHostBuilder(args).Build();
@@ -95,7 +115,6 @@ namespace Berg {
                     context.ItemReview.Attach(review);
                     context.Entry(ITEM_LIST[0]).State = EntityState.Modified;
                     await context.SaveChangesAsync();
-                    Console.WriteLine("");
                 }
             }
         }

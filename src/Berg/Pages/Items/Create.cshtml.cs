03b253c89482cfc9496a2019fc6256082693ee17
r-vu <46875289+r-vu@users.noreply.github.com>
2020-11-11 00:11:21 -0800

    Add image link to Create form
    Uploading a file will be implemented later using some sort of storage
    API like an S3 bucket.

--- src/Berg/Pages/Items/Create.cshtml.cs
+++ src/Berg/Pages/Items/Create.cshtml.cs
@@ -1,22 +1,33 @@
 ﻿using Berg.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
+
 
 namespace Berg.Pages.Items {
     public class CreateModel : PageModel {
         private readonly Berg.Data.BergContext _context;
 
+        [BindProperty]
+        public Item Item { get; set; }
+
+        [BindProperty(Name = "urlInputField")]
+        public string ImageUrl { get; set; }
+
+        [BindProperty(Name = "imageUploadInput")]
+        public ItemImage ImageFile { get; set; }
+
         public CreateModel(Berg.Data.BergContext context) {
             _context = context;
+
         }
 
         public IActionResult OnGet() {
             return Page();
         }
-
-        [BindProperty]
-        public Item Item { get; set; }
 
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://aka.ms/RazorPagesCRUD.
@@ -25,10 +36,29 @@ namespace Berg.Pages.Items {
                 return Page();
             }
 
+            //ImageFile = WebImage.GetImageFromRequest();
+            if (ImageUrl != null) {
+                Item.Image = new Uri(ImageUrl);
+            } else if (ImageFile != null) {
+                // Image API?
+
+                Item.Name = Item.Name + "_UsedUpload";
+            }
+
             _context.Item.Add(Item);
             await _context.SaveChangesAsync();
 
             return RedirectToPage("./Index");
         }
+
+        public async Task<IActionResult> OnPostUploadAsync() {
+            return Page();
+        }
+    }
+
+    public class ItemImage {
+
+        [Required]
+        public IFormFile FormFile { get; set; }
     }
 }
---
+++ src/Berg/wwwroot/js/Items_Create.js
@@ -0,0 +1,11 @@
+﻿"use strict"
+
+function enableUrl() {
+    document.getElementById("urlInputField").disabled = false;
+    document.getElementById("imageUploadInput").disabled = true;
+}
+
+function enableUpload() {
+    document.getElementById("urlInputField").disabled = true;
+    document.getElementById("imageUploadInput").disabled = false;
+}

2df24e8d6d30047ab3e84574d559fb6da578bbb9
r-vu <46875289+r-vu@users.noreply.github.com>
2020-12-02 19:25:24 -0800

    Begin creating logic for ReviewController tests

--- tests/Berg.Tests/ControllerTests/ReviewControllerTests.cs
+++ tests/Berg.Tests/ControllerTests/ReviewControllerTests.cs
@@ -1,17 +1,49 @@
-﻿using System.Threading.Tasks;
+﻿using Berg.Controllers.Api;
+using Berg.Data;
+using Berg.Models;
+using Berg.Tests.Mocks;
+using Microsoft.AspNetCore.Identity;
+using Moq;
+using RestSharp;
+using System.Threading.Tasks;
 using Xunit;
+using System.Net;
 
 namespace Berg.Tests.ControllerTests {
     public class ReviewControllerTests {
 
-        [Fact]
-        public async Task GetTest() {
+        private readonly ReviewController _controller;
+        private static readonly string API_URI = "https://localhost:44305/api/reviews/";
 
+        public ReviewControllerTests() {
+            MockDbSet<Item> itemDbSet = new MockDbSet<Item>();
+            MockDbSet<ItemReview> itemReviewDbSet = new MockDbSet<ItemReview>();
+            Mock<BergContext> context = new Mock<BergContext>();
+            Mock<UserManager<BergUser>> userManager = new Mock<UserManager<BergUser>>();
+            context.Setup(x => x.Item).Returns(itemDbSet.Object);
+            context.Setup(x => x.ItemReview).Returns(itemReviewDbSet.Object);
+
+            _controller = new ReviewController(context.Object, userManager.Object);
         }
 
         [Fact]
-        public async Task PostTest() {
+        public async Task ReviewController_GetExisting_ReturnsOk() {
+            RestClient client = new RestClient(API_URI);
+            client.Timeout = -1;
+            RestRequest request = new RestRequest(Method.GET);
+            IRestResponse response = client.Execute(request);
 
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task ReviewController_GetNonexisting_ReturnsNotFound() {
+            RestClient client = new RestClient(API_URI);
+            client.Timeout = -1;
+            RestRequest request = new RestRequest(Method.GET);
+            IRestResponse response = client.Execute(request);
+
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
         }
 
         [Fact]
---
+++ tests/Berg.Tests/Mocks/MockDbSet.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Berg.Tests.Mocks {
+    public class MockDbSet<TEntity> : Mock<DbSet<TEntity>> where TEntity : class {
+
+        public ICollection<TEntity> Store { get; set; }
+        public IQueryable<TEntity> Query { get; set; }
+
+        public MockDbSet(ICollection<TEntity> data = null) {
+            Store = data ?? new List<TEntity>();
+            Query = Store.AsQueryable();
+
+            As<IQueryable<TEntity>>().SetupGet(x => x.Provider).Returns(Query.Provider);
+            As<IQueryable<TEntity>>().SetupGet(x => x.Expression).Returns(Query.Expression);
+            As<IQueryable<TEntity>>().SetupGet(x => x.ElementType).Returns(Query.ElementType);
+            As<IQueryable<TEntity>>().Setup(x => x.GetEnumerator()).Returns(Query.GetEnumerator());
+
+            Setup(x => x.Add(It.IsAny<TEntity>())).Callback((TEntity entity) => Store.Add(entity));
+            Setup(x => x.Attach(It.IsAny<TEntity>())).Callback((TEntity entity) => Store.Add(entity));
+            Setup(x => x.Remove(It.IsAny<TEntity>())).Callback((TEntity entity) => Store.Remove(entity));
+
+            Setup(x => x.Find(It.IsAny<It.IsAnyType>()))
+                .Throws(new NotImplementedException("Implement find method according to the generic type"));
+
+        }
+    }
+}
